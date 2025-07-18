7668204089833be7b0fc0c01d045f85e51b85fcb
Anish Samantaray <anishsamantaray246@gmail.com>
2025-07-18 06:47:51 +0530

    migration from http to rest

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "api_endpoint" {
-  value = aws_apigatewayv2_api.guardianx_api.api_endpoint
+  value = "https://${aws_api_gateway_rest_api.guardianx_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}/"
 }
