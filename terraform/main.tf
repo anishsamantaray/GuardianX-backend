e7c8904799d0a3d7686a5f3c8ea7219d73cdd187
Anish Samantaray <anishsamantaray246@gmail.com>
2025-07-18 07:37:07 +0530

    open api doc fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-
 resource "aws_iam_role" "lambda_exec" {
   name = "guardianx-lambda-exec"
 
@@ -28,7 +27,6 @@ resource "aws_iam_role_policy_attachment" "lambda_dynamodb_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
-
 resource "aws_lambda_function" "guardianx" {
   function_name = "guardianx-fastapi"
   package_type  = "Image"
@@ -43,19 +41,17 @@ resource "aws_lambda_function" "guardianx" {
   }
 }
 
-
 resource "aws_api_gateway_rest_api" "guardianx_api" {
   name = "guardianx-rest-api"
 }
 
-
+# Catch-all proxy route
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.guardianx_api.id
   parent_id   = aws_api_gateway_rest_api.guardianx_api.root_resource_id
   path_part   = "{proxy+}"
 }
 
-# ANY method on proxy
 resource "aws_api_gateway_method" "proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.guardianx_api.id
   resource_id   = aws_api_gateway_resource.proxy.id
@@ -64,7 +60,6 @@ resource "aws_api_gateway_method" "proxy_method" {
   api_key_required = true
 }
 
-# Lambda integration
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.guardianx_api.id
   resource_id             = aws_api_gateway_resource.proxy.id
@@ -74,7 +69,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.guardianx.invoke_arn
 }
 
-
+# Public docs
 resource "aws_api_gateway_resource" "docs" {
   rest_api_id = aws_api_gateway_rest_api.guardianx_api.id
   parent_id   = aws_api_gateway_rest_api.guardianx_api.root_resource_id
@@ -89,20 +84,6 @@ resource "aws_api_gateway_method" "docs_method" {
   api_key_required = false
 }
 
-resource "aws_api_gateway_resource" "openapi_docs" {
-  rest_api_id = aws_api_gateway_rest_api.guardianx_api.id
-  parent_id   = aws_api_gateway_rest_api.guardianx_api.root_resource_id
-  path_part   = "openapi.json"
-}
-
-resource "aws_api_gateway_method" "openai_docs" {
-  rest_api_id   = aws_api_gateway_rest_api.guardianx_api.id
-  resource_id   = aws_api_gateway_resource.openapi_docs.id
-  http_method   = "GET"
-  authorization = "NONE"
-  api_key_required = false
-}
-
 resource "aws_api_gateway_integration" "docs_integration" {
   rest_api_id             = aws_api_gateway_rest_api.guardianx_api.id
   resource_id             = aws_api_gateway_resource.docs.id
@@ -112,11 +93,35 @@ resource "aws_api_gateway_integration" "docs_integration" {
   uri                     = aws_lambda_function.guardianx.invoke_arn
 }
 
+# openapi.json public route
+resource "aws_api_gateway_resource" "openapi_json" {
+  rest_api_id = aws_api_gateway_rest_api.guardianx_api.id
+  parent_id   = aws_api_gateway_rest_api.guardianx_api.root_resource_id
+  path_part   = "openapi.json"
+}
+
+resource "aws_api_gateway_method" "openapi_json_method" {
+  rest_api_id   = aws_api_gateway_rest_api.guardianx_api.id
+  resource_id   = aws_api_gateway_resource.openapi_json.id
+  http_method   = "GET"
+  authorization = "NONE"
+  api_key_required = false
+}
+
+resource "aws_api_gateway_integration" "openapi_json_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.guardianx_api.id
+  resource_id             = aws_api_gateway_resource.openapi_json.id
+  http_method             = aws_api_gateway_method.openapi_json_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.guardianx.invoke_arn
+}
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
-    aws_api_gateway_integration.docs_integration
+    aws_api_gateway_integration.docs_integration,
+    aws_api_gateway_integration.openapi_json_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.guardianx_api.id
 }
@@ -126,7 +131,6 @@ resource "aws_api_gateway_stage" "prod" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   stage_name    = "prod"
 }
-
 
 resource "aws_api_gateway_api_key" "guardianx_key" {
   name    = "guardianx-client-key"
@@ -140,19 +144,18 @@ resource "aws_api_gateway_usage_plan" "guardianx_plan" {
     api_id = aws_api_gateway_rest_api.guardianx_api.id
     stage  = aws_api_gateway_stage.prod.stage_name
 
-
-  }
-  throttle_settings {
+    throttle_settings {
       burst_limit = 100
       rate_limit  = 50
     }
+  }
 }
+
 resource "aws_api_gateway_usage_plan_key" "guardianx_usage_key" {
   key_id        = aws_api_gateway_api_key.guardianx_key.id
   key_type      = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.guardianx_plan.id
 }
-
 
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
