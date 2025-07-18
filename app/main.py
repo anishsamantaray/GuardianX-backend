691bd93160bc933928ffe07493c27c883008f6f3
Anish Samantaray <anishsamantaray246@gmail.com>
2025-07-18 07:46:01 +0530

    open api doc fixed

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request ,HTTPException
 from app.routes.user import router as user_router
 from app.routes.maps import router as maps_router
 from app.routes.ally import router as ally_router
@@ -8,6 +8,18 @@ from app.routes.incident import router as incident_router
 from mangum import Mangum
 
 app = FastAPI(title="GuardianX Backend")
+
+@app.middleware("http")
+async def check_api_key(request: Request, call_next):
+    exempt_paths = ["/docs", "/openapi.json", "/redoc"]
+    if request.url.path in exempt_paths:
+        return await call_next(request)
+
+    api_key = request.headers.get("x-api-key")
+    if api_key != "your-secret-api-key":
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+    return await call_next(request)
 app.include_router(user_router)
 app.include_router(maps_router)
 app.include_router(ally_router)
