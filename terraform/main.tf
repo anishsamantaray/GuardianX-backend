793b213d17ef619d6b837595d59bfcbc068729f8
Anish Samantaray <anishsamantaray246@gmail.com>
2025-07-18 07:21:26 +0530

    added openai docs allong with swagger

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,6 +89,20 @@ resource "aws_api_gateway_method" "docs_method" {
   api_key_required = false
 }
 
+resource "aws_api_gateway_resource" "openai_docs" {
+  rest_api_id = aws_api_gateway_rest_api.guardianx_api.id
+  parent_id   = aws_api_gateway_rest_api.guardianx_api.root_resource_id
+  path_part   = "openapi.json"
+}
+
+resource "aws_api_gateway_method" "openai_docs" {
+  rest_api_id   = aws_api_gateway_rest_api.guardianx_api.id
+  resource_id   = aws_api_gateway_resource.docs.id
+  http_method   = "GET"
+  authorization = "NONE"
+  api_key_required = false
+}
+
 resource "aws_api_gateway_integration" "docs_integration" {
   rest_api_id             = aws_api_gateway_rest_api.guardianx_api.id
   resource_id             = aws_api_gateway_resource.docs.id
