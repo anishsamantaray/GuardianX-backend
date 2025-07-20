62ca86d01cfc4e2cb1a6908f61b13e85a0265279
Anish Samantaray <anishsamantaray246@gmail.com>
2025-07-20 10:34:02 +0530

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -6,22 +6,17 @@ from app.routes.sos import router as sos_router
 from app.routes.incident import router as incident_router
 import os
 from mangum import Mangum
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="GuardianX Backend")
 
-@app.middleware("http")
-async def check_api_key(request: Request, call_next):
-    # exempt_paths = ["/docs", "/openapi.json", "/redoc"]
-    # if any(request.url.path.endswith(path) for path in exempt_paths):
-    #     return await call_next(request)
-    #
-    # api_key = request.headers.get("x-api-key")
-    # expected_key = os.environ.get("GUARDIANX_API_KEY")
-    #
-    # if api_key != expected_key:
-    #     raise HTTPException(status_code=403, detail="Forbidden")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # or ["https://your-frontend.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    return await call_next(request)
 app.include_router(user_router)
 app.include_router(maps_router)
 app.include_router(ally_router)
