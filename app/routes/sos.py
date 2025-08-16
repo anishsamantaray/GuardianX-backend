b75e83a542e05ba4e61a7dcd4a70687a240d18a0
Anish Samantaray <anishsamantaray246@gmail.com>
2025-08-16 09:08:01 +0530

    fix:float to decimal for dynamo storage

--- app/routes/sos.py
+++ app/routes/sos.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-
+from decimal import Decimal
 from app.schemas.sos_schemas import SOSTriggerRequest, SOSHeartbeatRequest, SOSEndRequest
 from app.utils.sos_utils import (
     trigger_sos_with_alert,
@@ -11,22 +11,32 @@ router = APIRouter(prefix="/sos", tags=["sos"])
 
 @router.post("/trigger")
 async def trigger_sos_event(request: SOSTriggerRequest):
+    location_dict = request.location.model_dump() if hasattr(request.location, "model_dump") else request.location.dict()
+
+    location_dict["latitude"] = Decimal(str(location_dict["latitude"]))
+    location_dict["longitude"] = Decimal(str(location_dict["longitude"]))
+
     await trigger_sos_with_alert(
         request.email,
-        request.location.model_dump() if hasattr(request.location, "model_dump") else request.location.dict(),
+        location_dict,
         request.timestamp
     )
     return {"status": "sos_triggered_and_alerts_enqueued"}
 
+
 @router.post("/heartbeat")
 async def update_sos_heartbeat(request: SOSHeartbeatRequest):
+    location_dict = request.location.model_dump() if hasattr(request.location, "model_dump") else request.location.dict()
+
+    location_dict["latitude"] = Decimal(str(location_dict["latitude"]))
+    location_dict["longitude"] = Decimal(str(location_dict["longitude"]))
+
     await update_heartbeat_with_alert(
         request.email,
         request.timestamp,
-        request.location.model_dump() if hasattr(request.location, "model_dump") else request.location.dict()
+        location_dict
     )
     return {"status": "heartbeat_updated_and_alerts_enqueued"}
-
 @router.post("/end")
 async def end_sos_event(request: SOSEndRequest):
     end_sos(request.email, request.timestamp)
