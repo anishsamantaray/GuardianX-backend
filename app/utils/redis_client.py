6ac7a5c5727c7769af79a6bf95a08e3a9be54c75
Anish Samantaray <anishsamantaray246@gmail.com>
2025-10-21 23:40:35 +0530

    fix:ssl issue

--- app/utils/redis_client.py
+++ app/utils/redis_client.py
@@ -6,10 +6,11 @@ load_dotenv()
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 16074))
-
+REDIS_PASSWORD= os.getenv("REDIS_PASSWORD")
 redis_client = redis.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
-    ssl=False,
     decode_responses=True,
+    username="default",
+    password=REDIS_PASSWORD
 )
