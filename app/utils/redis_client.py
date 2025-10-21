1b0624eb590acb2a1031869f870235cbd28835f1
Anish Samantaray <anishsamantaray246@gmail.com>
2025-10-21 22:57:07 +0530

    fix:port issue

--- app/utils/redis_client.py
+++ app/utils/redis_client.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_PORT = int(os.getenv("REDIS_PORT", 16074))
 
 redis_client = redis.Redis(
     host=REDIS_HOST,
+    port=REDIS_PORT,
     ssl=True,
     decode_responses=True,
 )
